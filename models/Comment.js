4abaa3dca4f237e9d6f426cad9698d56f15924bd
Nick Eggleston <nickeggleston@yahoo.com>
2024-02-26 15:20:51 -0500

    Comment.js DONE

---
+++ models/Comment.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            }
+        },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
