0d8af0a15ec6612c637c5a2cd779598d9c432a62
Nick Eggleston <nickeggleston@yahoo.com>
2024-02-26 15:24:35 -0500

    Post.js DONE

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const { Models, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Models {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    },
+        {
+            sequelize,
+            timestamps: true,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'post'
+        }
+    );
+
+module.exports = Post;
